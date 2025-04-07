93a079271905edcec969a4219d3bf17720957f6f
gabzaf <gabriel.affonsoo@gmail.com>
2025-04-07 20:11:48 +0100

    Done: ex00 - module_2

--- module_2/ex00/Fixed.hpp
+++ module_2/ex00/Fixed.hpp
@@ -6,7 +6,17 @@
 
 class Fixed
 {
+	private:
+		int			rawBits;
+		static const int	fractionalBits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed &operator = (Fixed const &other);
+		~Fixed();
 
+		int	getRawBits(void) const;
+		void	setRawBits(int const raw);
 };
 
 #endif
