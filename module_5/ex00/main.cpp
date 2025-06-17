cd8471dbfe211d94c90675ad5c131ad558c1fe8a
Gabriel Amado Xavier Oliveira Affonso <gabriel.affonsoo@gmail.com>
2025-06-17 19:17:29 +0100

    Doing: ex00 main function

--- module_5/ex00/main.cpp
+++ module_5/ex00/main.cpp
@@ -2,13 +2,11 @@
 
 int	main()
 {
-	std::cout << " --- Test - try decrement ---" << std::endl;
-	try 
+	std::cout << std::endl << " --- Test -> Instantiating Bureaucrat with impossible grade ( > 150) --- " << std::endl << std::endl;
+	try
 	{
-		Bureaucrat a("Ney", 149);
+		Bureaucrat a("Rony", 155);
 		std::cout << a << std::endl;
-		a.decrementGrade();
-		a.decrementGrade();
 	}
 	catch(Bureaucrat::GradeTooHighException &b)
 	{
@@ -18,21 +16,73 @@ int	main()
 	{
  		std::cout<< "Exception caught - "<< b.what() << std::endl;
 	}
-
-	std::cout << " --- Test - try increment ---" << std::endl;
+	std::cout << std::endl << " --- Test -> Instantiating Bureaucrat with impossible grade ( < 1) ---" << std::endl << std::endl;
+	try
+	{
+		Bureaucrat c("Tony", 0);
+		std::cout << c << std::endl;
+	}
+	catch(Bureaucrat::GradeTooHighException &d)
+	{
+		std::cout<< "Exception caught - "<< d.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException &d)
+	{
+ 		std::cout<< "Exception caught - "<< d.what() << std::endl;
+	}	
+	std::cout << std::endl << " --- Test -> Basic methods ---" << std::endl << std::endl;
 	try 
 	{
-		Bureaucrat c("Messi", 10);
-		std::cout << c << std::endl;
- 		c.incrementGrade();
+		Bureaucrat e("Ney", 149);
+		std::cout << e << std::endl;
+		std::cout << e.getName() << std::endl;
+		std::cout << e.getGrade() << std::endl;
 	}
-	catch(Bureaucrat::GradeTooHighException &e)
+	catch(Bureaucrat::GradeTooHighException &f)
 	{
-		std::cout << "Exception caught - " << e.what() << std::endl;
+		std::cout<< "Exception caught - "<< f.what() << std::endl;
 	}
-	catch(Bureaucrat::GradeTooLowException &e)
+	catch(Bureaucrat::GradeTooLowException &f)
 	{
-		std::cout << "Exception caught - " << e.what() << std::endl;
+ 		std::cout<< "Exception caught - "<< f.what() << std::endl;
+	}
+	std::cout << std::endl << " --- Test -> Try increment ---" << std::endl << std::endl;
+	try 
+	{
+		Bureaucrat g("Messi", 2);
+		std::cout << g << std::endl;
+ 		g.incrementGrade();
+		std::cout << g.getGrade() << std::endl;
+		g.incrementGrade();
+		std::cout << g.getGrade() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooHighException &h)
+	{
+		std::cout << "Exception caught - " << h.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException &h)
+	{
+		std::cout << "Exception caught - " << h.what() << std::endl;
+	}
+	std::cout << std::endl << " --- Test -> Try decrement ---" << std::endl << std::endl;
+	try 
+	{
+		Bureaucrat i("Messi", 148);
+		std::cout << i << std::endl;
+ 		i.decrementGrade();
+		std::cout << i.getGrade() << std::endl;
+		i.decrementGrade();
+		std::cout << i.getGrade() << std::endl;
+		i.decrementGrade();
+		std::cout << i.getGrade() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooHighException &j)
+	{
+		std::cout << "Exception caught - " << j.what() << std::endl;
+	}
+	catch(Bureaucrat::GradeTooLowException &j)
+	{
+		std::cout << "Exception caught - " << j.what() << std::endl;
 	}
 	return (0);
 }
