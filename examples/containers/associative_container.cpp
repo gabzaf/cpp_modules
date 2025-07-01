090155de2f9b3f1224a0a570892f09134af2d8ba
rei <gabriel.affonsoo@gmail.com>
2025-07-01 16:31:18 +0100

    Studying

---
+++ examples/containers/associative_container.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <set>
+
+void    associative_container_set()
+{
+    std::cout << "set" << std::endl;
+    std::set<int> s = {5, 3, 8, 1, 2, 2, 0, 30};
+    for (int i : s)
+    {
+        std::cout << i << std::endl;
+    }
+}
+
+void    associative_container_map()
+{
+    std::map<std::string, int> m;
+    std::cout << "map" << std::endl;
+    m["one"] = 1;
+    m["three"] = 3;
+    m["two"] = 2;
+
+    //std::cout << m["one"] << std::endl;
+    for (const auto& pair : m)
+    {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+}
+
+int main()
+{
+    associative_container_map();
+    associative_container_set();
+    return (0);
+}
