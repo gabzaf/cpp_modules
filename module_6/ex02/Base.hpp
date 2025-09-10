ca06572c6f22ef965aad9c39af43f44348c3063b
rei <gabriel.affonsoo@gmail.com>
2025-09-10 12:07:30 +0100

    Done: ex02

---
+++ module_6/ex02/Base.hpp
@@ -0,0 +1,14 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+# include <iostream>
+# include <cstdlib>
+# include <ctime>
+# include <exception>
+
+class Base {
+	public:
+		virtual ~Base(void);
+};
+
+#endif 
