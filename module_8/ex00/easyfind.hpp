8451d89701c19d12d8970d61379ab7bf0683d4af
rei <gabriel.affonsoo@gmail.com>
2025-11-03 12:59:36 +0000

    Done: ex00 but need more tests

---
+++ module_8/ex00/easyfind.hpp
@@ -0,0 +1,20 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <exception>
+
+template <typename T>
+
+typename T::iterator easyfind(T &container, int nbr)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), nbr);
+    if (it == container.end())
+        throw std::runtime_error("No occurrence of the input.");
+    else
+        std::cout << "Input found" << std::endl;
+    return (it);
+}
+
+#endif
---
+++ module_8/ex00/main.cpp
@@ -0,0 +1,34 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+
+int main()
+{
+    int arr[] = {-1345, -3, 0, 1, 42, 1234142};
+    std::vector<int> v(arr, arr + sizeof(arr)/sizeof(arr[0]));
+    std::list<int> l(arr, arr + sizeof(arr)/sizeof(arr[0]));
+
+    std::cout << "Vector" << std::endl << std::endl;
+    try
+    {
+        easyfind(v, -1345);
+        easyfind(v, -1);
+        easyfind(v, 42);
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl << "List" << std::endl << std::endl;
+    try
+    {
+        easyfind(l, -1345);
+        easyfind(l, 42);
+        easyfind(l, -1);
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
