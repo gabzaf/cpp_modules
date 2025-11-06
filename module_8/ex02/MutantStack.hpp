627667b52990644eb38f4dafd1bbece1089e75d5
rei <gabriel.affonsoo@gmail.com>
2025-11-06 12:44:54 +0000

    Doing: ex02

--- module_8/ex02/MutantStack.hpp
+++ module_8/ex02/MutantStack.hpp
@@ -1,15 +1,30 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <stack>
+#include <iterator>
+
 template <typename T>
 
 class MutantStack : public std::stack<T>
 {
-    private:
-
     public:
+        MutantStack();
+        MutantStack(const MutantStack &other);
+        MutantStack<T> &operator=(const MutantStack<T> &other);
+        ~MutantStack();
 
-}
+        typedef typename std::stack<T>::container_type wraped_cntnr;
+        typedef typename wraped_cntr::iterator it;
+        typedef typename wraped_cntnr::const_iterator const_iterator;
 
+        iterator begin();
+        iterator end();
+        const iterator begin();
+        const iterator end();
+};
+
+#include "MutantStack.tpp"
 
 #endif
